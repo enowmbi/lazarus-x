531f8605d065a2fb337a59a5615858a140cd3e6e
Enow Mbi <benowmbi@yahoo.com>
2022-06-07 23:24:30 +0100

    Refactor archived_employee.rb

--- spec/models/archived_employee_spec.rb
+++ spec/models/archived_employee_spec.rb
@@ -5,9 +5,17 @@ RSpec.describe ArchivedEmployee, type: :model do
     it { is_expected.to have_db_column(:first_name).of_type(:string) }
     it { is_expected.to have_db_column(:middle_name).of_type(:string) }
     it { is_expected.to have_db_column(:last_name).of_type(:string) }
+    it { is_expected.to have_before_action(:status_false).on(:save) }
+    it { is_expected.to have_before_save(:status_false) }
   end
 
   describe "Associations" do
 
   end
+
+  describe "#full_name" do
+    
+  end
+
+
 end
