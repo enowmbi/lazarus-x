ba08e46cabac1513e19f941487bfc4c096469ef5
Enow Mbi <benowmbi@yahoo.com>
2022-06-20 16:29:01 +0100

    Refactor finance fee collection discount

--- app/models/finance/fee_collection_discount.rb
+++ app/models/finance/fee_collection_discount.rb
@@ -1,31 +1,34 @@
-#Fedena
-#Copyright 2011 Foradian Technologies Private Limited
+# Fedena
+# Copyright 2011 Foradian Technologies Private Limited
 #
-#This product includes software developed at
-#Project Fedena - http://www.projectfedena.org/
+# This product includes software developed at
+# Project Fedena - http://www.projectfedena.org/
 #
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #  http://www.apache.org/licenses/LICENSE-2.0
 #
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-class FeeCollectionDiscount < ApplicationRecord
+module Finance
+  class FeeCollectionDiscount < ApplicationRecord
+    belongs_to :finance_fee_collection, class_name: "Finance::FinanceFeeCollection"
 
-  def category_name
-    c =StudentCategory.find(self.receiver_id)
-    c.name unless c.nil?
+    def category_name
+      c = StudentCategory.find(receiver_id)
+
+      c&.name
+    end
+
+    def student_name
+      s = Student.find(receiver_id)
+      "#{s.first_name} (#{s.admission_no})" unless s.nil?
+    end
   end
-
-  def student_name
-    s =Student.find(self.receiver_id)
-    "#{s.first_name} (#{s.admission_no})" unless s.nil?
-  end
-
 end
