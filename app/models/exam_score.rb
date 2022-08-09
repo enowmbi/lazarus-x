941a5413cdef466eb6a5d28099a4a2078cd45d8d
Enow Mbi <benowmbi@yahoo.com>
2022-08-09 14:59:09 +0100

    Refactor exam_score.rb -- fix rubocop offenses

--- app/models/exam_score.rb
+++ app/models/exam_score.rb
@@ -1,20 +1,20 @@
-#Fedena
-#Copyright 2011 Foradian Technologies Private Limited
+# Fedena
+# Copyright 2011 Foradian Technologies Private Limited
 #
-#This product includes software developed at
-#Project Fedena - http://www.projectfedena.org/
+# This product includes software developed at
+# Project Fedena - http://www.projectfedena.org/
 #
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #  http://www.apache.org/licenses/LICENSE-2.0
 #
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 class ExamScore < ApplicationRecord
   belongs_to :student
@@ -24,84 +24,79 @@ class ExamScore < ApplicationRecord
   before_save :calculate_grade
   before_save :check_existing
 
-
-  validates_presence_of :student_id
-  validates_presence_of :exam_id,:message =>  "Name/Batch Name/Subject Code is invalid"
-  validates_numericality_of :marks,:allow_nil => true
-  
-
+  validates :student_id, presence: true
+  validates :exam_id, presence: true, message: "Name/Batch Name/Subject Code is invalid"
+  validates :marks, numericality: true, allow_nil: true
 
   def check_existing
-    exam_score = ExamScore.find(:first,:conditions => {:exam_id => self.exam_id,:student_id => self.student_id})
+    exam_score = ExamScore.where(exam_id: exam_id, student_id: student_id).first
     if exam_score
       self.id = exam_score.id
-      self.instance_variable_set("@new_record",false)    #If the record exists,then make the new record as a copy of the existing one and allow rails to chhose
-                                                         #the update operation instead of insert. 
+
+      # If the record exists,then make the new record as a copy of the existing one and allow rails to chhose
+      # the update operation instead of insert.
+      instance_variable_set("@new_record", false)
     end
-    return true
+    true
   end
 
-  
   def validate
-    unless self.marks.nil?
-      unless self.exam.nil?
-        if self.exam.maximum_marks.to_f < self.marks.to_f
-          errors.add('marks','cannot be greater than maximum marks')
-          return false
-        else
-          return true
-        end
-      end
+    return if marks.nil?
+
+    return if exam.nil?
+
+    if exam.maximum_marks.to_f < marks.to_f
+      errors.add('marks', 'cannot be greater than maximum marks')
+      false
+    else
+      true
     end
   end
 
-  
   def calculate_percentage
-    percentage = self.marks.to_f * 100 / self.exam.maximum_marks.to_f
+    marks.to_f * 100 / exam.maximum_marks.to_f
   end
 
-  def grouped_exam_subject_total(subject,student,type,batch = "")
-    if batch == ""
-      batch = student.batch.id
-    end
+  def grouped_exam_subject_total(subject, student, type, batch = "")
+    batch = student.batch.id if batch == ""
     if type == 'grouped'
-      grouped_exams = GroupedExam.find_all_by_batch_id(batch)
+      grouped_exams = GroupedExam.where(batch_id: batch)
       exam_groups = []
       grouped_exams.each do |x|
         eg = ExamGroup.find(x.exam_group_id)
         exam_groups.push ExamGroup.find(x.exam_group_id)
       end
     else
-      exam_groups = ExamGroup.find_all_by_batch_id(batch)
+      exam_groups = ExamGroup.where(batch_id: batch)
     end
     total_marks = 0
     exam_groups.each do |exam_group|
-      unless exam_group.exam_type == 'Grades'
-        exam = Exam.find_by_subject_id_and_exam_group_id(subject.id,exam_group.id)
-        unless exam.nil?
-          exam_score = ExamScore.find_by_student_id(student.id, :conditions=>{:exam_id=>exam.id})
-          marks = exam_score.nil? ? 0 : exam_score.marks.nil? ? 0 : exam_score.marks
-          total_marks = total_marks + marks unless exam_score.nil?
-        end
-      end
+      next if exam_group.exam_type == 'Grades'
+
+      exam = Exam.find_by(subject_id: subject.id, exam_group_id: exam_group.id)
+      next if exam.nil?
+
+      exam_score = ExamScore.find_by(student_id: student.id, exam_id: exam.id)
+      marks = if exam_score.nil?
+                0
+              else
+                exam_score.marks.nil? ? 0 : exam_score.marks
+              end
+      total_marks += marks unless exam_score.nil?
     end
     total_marks
   end
 
-  
-
-  def batch_wise_aggregate(student,batch)
-    check = ExamGroup.find_all_by_batch_id(batch.id)
+  def batch_wise_aggregate(student, batch)
+    check = ExamGroup.where(batch_id: batch.id)
     var = []
     check.each do |x|
-      if x.exam_type == 'Grades'
-        var << 1
-      end
+      var << 1 if x.exam_type == 'Grades'
     end
     if var.empty?
-      grouped_exam = GroupedExam.find_all_by_batch_id(batch.id)
+      grouped_exam = GroupedExam.where(batch_id: batch.id)
       if grouped_exam.empty?
-        exam_groups = ExamGroup.find_all_by_batch_id(batch.id)
+        exam_groups = ExamGroup.where(batch_id: batch.id)
       else
         exam_groups = []
         grouped_exam.each do |x|
@@ -112,30 +107,30 @@ class ExamScore < ApplicationRecord
       max_total = 0
       marks_total = 0
       exam_groups.each do |exam_group|
-        max_total = max_total + exam_group.total_marks(student)[1]
-        marks_total = marks_total + exam_group.total_marks(student)[0]
+        max_total += exam_group.total_marks(student)[1]
+        marks_total += exam_group.total_marks(student)[0]
       end
-      aggr = (marks_total*100/max_total) unless max_total==0
+      (marks_total * 100 / max_total) unless max_total.zero?
     else
-      aggr = 'nil'
+      'nil'
     end
-    
   end
 
   private
+
   def calculate_grade
     exam = self.exam
     exam_group = exam.exam_group
     exam_type = exam_group.exam_type
-    unless exam_type == 'Grades'
-      unless self.marks.nil?
-        percent_score = self.marks.to_i * 100 / self.exam.maximum_marks
-        grade = GradingLevel.percentage_to_grade(percent_score, self.exam.exam_group.batch_id)
-        self.grading_level_id = grade.id if exam_type == 'MarksAndGrades'
-      else
-        self.grading_level_id = nil
-      end
+
+    return if exam_type == 'Grades'
+
+    if marks
+      self.grading_level_id = nil
+    else
+      percent_score = marks.to_i * 100 / self.exam.maximum_marks
+      grade = GradingLevel.percentage_to_grade(percent_score, self.exam.exam_group.batch_id)
+      self.grading_level_id = grade.id if exam_type == 'MarksAndGrades'
     end
   end
-
 end
