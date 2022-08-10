2ad9f765293853ad3e2a5d1f3ef72a3e76fa2400
Enow Mbi <benowmbi@yahoo.com>
2022-08-10 23:47:42 +0100

    Fix rubocop offenses in observation_group.rb

--- app/models/observation_group.rb
+++ app/models/observation_group.rb
@@ -1,35 +1,32 @@
-#Fedena
-#Copyright 2011 Foradian Technologies Private Limited
+# Fedena
+# Copyright 2011 Foradian Technologies Private Limited
 #
-#This product includes software developed at
-#Project Fedena - http://www.projectfedena.org/
+# This product includes software developed at
+# Project Fedena - http://www.projectfedena.org/
 #
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #  http://www.apache.org/licenses/LICENSE-2.0
 #
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 class ObservationGroup < ApplicationRecord
-  has_many                  :observations
-  has_many                  :descriptive_indicators, :through=>:observations
-  belongs_to                :cce_grade_set
-  has_and_belongs_to_many   :courses
+  has_many :observations, dependent: :destroy
+  has_many :descriptive_indicators, through: :observations
+  belongs_to :cce_grade_set
+  has_and_belongs_to_many :courses
 
-  named_scope :active,:conditions=>{:is_deleted=>false}
-  
-  OBSERVATION_KINDS={'0'=>'Scholastic','1'=>'Co Scholastic Activity','3'=>'Co Scholastic Area'}
+  scope :active, -> { where(is_deleted: false) }
 
-  validates_presence_of :name
-  validates_presence_of :header_name
-  validates_presence_of :observation_kind #,:max_marks
-  def validate
-    errors.add_to_base("CCE grade set can't be blank") if self.cce_grade_set_id.blank?
-    errors.add_to_base("Description can't be blank") if self.desc.blank?
-  end
+  OBSERVATION_KINDS = { '0' => 'Scholastic', '1' => 'Co Scholastic Activity', '3' => 'Co Scholastic Area' }.freeze
+
+  validates :name, presence: true
+  validates :header_name, presence: true
+  validates :observation_kind, presence: true # ,:max_marks
+  validates :desc, presence: true
 end
