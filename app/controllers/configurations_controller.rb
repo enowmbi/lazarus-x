1f43ee5692df3fe3c31dd207c5d305a78693439e
Enow Mbi <benowmbi@yahoo.com>
2023-06-29 16:25:57 +0100

    Rename configuration_controller to configurations_controller following Rails convention

--- app/controllers/configurations_controller.rb
+++ app/controllers/configurations_controller.rb
@@ -16,7 +16,7 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 
-class ConfigurationController < ApplicationController
+class ConfigurationsController < ApplicationController
   before_filter :login_required
   filter_access_to :all
 
