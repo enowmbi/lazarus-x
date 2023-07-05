f1bb48f3506dcc058f9f806b18b375d5fb79b391
Enow Mbi <benowmbi@yahoo.com>
2023-07-05 07:15:05 +0100

    Remove devise.en.yml and schema.rb and create drop_devise_user_table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_200750) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_05_061324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -1354,46 +1354,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_200750) do
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_user_events_on_event_id"
     t.index ["user_id"], name: "index_user_events_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.boolean "admin"
-    t.boolean "student"
-    t.boolean "employee"
-    t.string "hashed_password"
-    t.string "salt"
-    t.string "reset_password_code"
-    t.datetime "reset_password_code_until"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "parent"
-    t.boolean "is_first_login"
-    t.boolean "is_deleted", default: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "weekdays", force: :cascade do |t|
