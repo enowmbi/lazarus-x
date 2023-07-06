8c59c73488627bd07edfd6a83fde4f34b2360eda
Enow Mbi <benowmbi@yahoo.com>
2023-07-06 10:34:44 +0100

    Fix rubocop offenses -- replace ^ with \A and $ with \z in regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,34 +1,36 @@
 class User < ApplicationRecord
   attr_accessor :password, :role, :old_password, :new_password, :confirm_password
 
-  validates_uniqueness_of :username, :scope=> [:is_deleted], :if=> 'is_deleted == false' #, :email
-  validates_length_of     :username, :within => 1..20
-  validates_length_of     :password, :within => 4..40, :allow_nil => true
-  validates_format_of     :username, :with => /^[A-Z0-9_-]*$/i,
-    :message => "#{t('must_contain_only_letters')}"
-  validates_format_of     :email, :with => /^[A-Z0-9._%-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i, :allow_blank=>true,
-    :message => "#{t('must_be_a_valid_email_address')}"
-  validates_presence_of   :role , :on=>:create
-  validates_presence_of   :password, :on => :create
+  validates :username, uniqueness: { scope: [:is_deleted], unless: :is_deleted }
+  validates     :username, length: { within: 1..20 }
+  validates     :password, length: { within: 4..40, allow_nil: true }
+  validates     :username, format: { with: /\A[A-Z0-9_-]*\z/i,
+                                     message: I18n.t('must_contain_only_letters').to_s }
+  validates     :email, format: { with: /\A[A-Z0-9._%-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}\z/i, allow_blank: true,
+                                  message: I18n.t('must_be_a_valid_email_address').to_s }
+  validates   :role, presence: { on: :create }
+  validates   :password, presence: { on: :create }
 
   has_and_belongs_to_many :privileges
   has_many  :user_events
-  has_many  :events, :through=>:user_events
-  has_one :student_record, :class_name=>"Student", :foreign_key=>"user_id"
-  has_one :employee_record, :class_name=>"Employee", :foreign_key=>"user_id"
+  has_many  :events, through: :user_events
+  has_one :student_record, class_name: "Student"
+  has_one :employee_record, class_name: "Employee"
 
-  named_scope :active, :conditions => { :is_deleted => false }
-  named_scope :inactive, :conditions => { :is_deleted => true }
+  named_scope :active, conditions: { is_deleted: false }
+  named_scope :inactive, conditions: { is_deleted: true }
 
   def before_save
-    self.salt = random_string(8) if self.salt == nil
-    self.hashed_password = Digest::SHA1.hexdigest(self.salt + self.password) unless self.password.nil?
-    if self.new_record?
-      self.admin, self.student, self.employee = false, false, false
-      self.admin    = true if self.role == 'Admin'
-      self.student  = true if self.role == 'Student'
-      self.employee = true if self.role == 'Employee'
-      self.parent = true if self.role == 'Parent'
+    self.salt = random_string(8) if salt.nil?
+    self.hashed_password = Digest::SHA1.hexdigest(salt + password) unless password.nil?
+    if new_record?
+      self.admin = false
+      self.student = false
+      self.employee = false
+      self.admin    = true if role == 'Admin'
+      self.student  = true if role == 'Student'
+      self.employee = true if role == 'Employee'
+      self.parent = true if role == 'Parent'
       self.is_first_login = true
     end
   end
@@ -38,19 +40,17 @@ class User < ApplicationRecord
   end
 
   def check_reminders
-    reminders =[]
-    reminders = Reminder.find(:all , :conditions => ["recipient = '#{self.id}'"])
+    reminders = []
+    reminders = Reminder.find(:all, conditions: ["recipient = '#{id}'"])
     count = 0
     reminders.each do |r|
-      unless r.is_read
-        count += 1
-      end
+      count += 1 unless r.is_read
     end
-    return count
+    count
   end
 
-  def self.authenticate?(username, password)
-    u = User.find_by_username username
+  def self.authenticate?(_username, password)
+    u = User.find_by username:
     u.hashed_password == Digest::SHA1.hexdigest(u.salt + password)
   end
 
@@ -62,11 +62,12 @@ class User < ApplicationRecord
   end
 
   def role_name
-    return "#{t('admin')}" if self.admin?
-    return "#{t('student_text')}" if self.student?
-    return "#{t('employee_text')}" if self.employee?
-    return "#{t('parent')}" if self.parent?
-    return nil
+    return I18n.t('admin').to_s if admin?
+    return I18n.t('student_text').to_s if student?
+    return I18n.t('employee_text').to_s if employee?
+    return I18n.t('parent').to_s if parent?
+
+    nil
   end
 
   def role_symbols
@@ -74,9 +75,9 @@ class User < ApplicationRecord
     @privilge_symbols ||= privileges.map { |privilege| prv << privilege.name.underscore.to_sym }
 
     if admin?
-      return [:admin] + prv
+      [:admin] + prv
     elsif student?
-      return [:student] + prv
+      [:student] + prv
     elsif employee?
       employee = employee_record
       unless employee.nil?
@@ -84,25 +85,23 @@ class User < ApplicationRecord
           prv << :subject_attendance if Configuration.get_config_value('StudentAttendanceType') == 'SubjectWise'
           prv << :subject_exam
         end
-        if Batch.active.collect(&:employee_id).include?(employee.id.to_s)
-          prv << :view_results
-        end
+        prv << :view_results if Batch.active.collect(&:employee_id).include?(employee.id.to_s)
       end
-      return [:employee] + prv
+      [:employee] + prv
     elsif parent?
-      return [:parent] + prv
+      [:parent] + prv
     else
-      return prv
+      prv
     end
   end
 
   def clear_menu_cache
-    Rails.cache.delete("user_main_menu#{self.id}")
-    Rails.cache.delete("user_autocomplete_menu#{self.id}")
+    Rails.cache.delete("user_main_menu#{id}")
+    Rails.cache.delete("user_autocomplete_menu#{id}")
   end
 
   def parent_record
-    Student.find_by_admission_no(self.username[1..self.username.length])
+    Student.find_by(admission_no: username[1..username.length])
   end
 
   def has_subject_in_batch(b)
@@ -110,58 +109,82 @@ class User < ApplicationRecord
   end
 
   def days_events(date)
-    all_events=[]
-    case(role_name)
+    all_events = []
+    case role_name
     when "Admin"
-      all_events= Event.find(:all, :conditions => ["? between date(events.start_date) and date(events.end_date)" ,date])
+      all_events = Event.find(:all,
+                              conditions: ["? between date(events.start_date) and date(events.end_date)", date])
     when "Student"
-      all_events += events.all(:conditions=>["? between date(events.start_date) and date(events.end_date)",date])
-      all_events += student_record.batch.events.all(:conditions=>["? between date(events.start_date) and date(events.end_date)",date])
-      all_events += Event.all(:conditions=>["(? between date(events.start_date) and date(events.end_date)) and is_common = true",date])
+      all_events += events.all(conditions: ["? between date(events.start_date) and date(events.end_date)", date])
+      all_events += student_record.batch.events.all(conditions: [
+                                                      "? between date(events.start_date) and date(events.end_date)", date
+                                                    ])
+      all_events += Event.all(conditions: [
+                                "(? between date(events.start_date) and date(events.end_date)) and is_common = true", date
+                              ])
     when "Parent"
-      all_events += events.all(:conditions=>["? between date(events.start_date) and date(events.end_date)",date])
-      all_events += parent_record.user.events.all(:conditions=>["? between date(events.start_date) and date(events.end_date)",date])
-      all_events += parent_record.batch.events.all(:conditions=>["? between date(events.start_date) and date(events.end_date)",date])
-      all_events += Event.all(:conditions=>["(? between date(events.start_date) and date(events.end_date)) and is_common = true",date])
+      all_events += events.all(conditions: ["? between date(events.start_date) and date(events.end_date)", date])
+      all_events += parent_record.user.events.all(conditions: [
+                                                    "? between date(events.start_date) and date(events.end_date)", date
+                                                  ])
+      all_events += parent_record.batch.events.all(conditions: [
+                                                     "? between date(events.start_date) and date(events.end_date)", date
+                                                   ])
+      all_events += Event.all(conditions: [
+                                "(? between date(events.start_date) and date(events.end_date)) and is_common = true", date
+                              ])
     when "Employee"
-      all_events += events.all(:conditions=>["? between events.start_date and events.end_date",date])
-      all_events += employee_record.employee_department.events.all(:conditions=>["? between date(events.start_date) and date(events.end_date)",date])
-      all_events += Event.all(:conditions=>["(? between date(events.start_date) and date(events.end_date)) and is_exam = true",date])
-      all_events += Event.all(:conditions=>["(? between date(events.start_date) and date(events.end_date)) and is_common = true",date])
+      all_events += events.all(conditions: ["? between events.start_date and events.end_date", date])
+      all_events += employee_record.employee_department.events.all(conditions: [
+                                                                     "? between date(events.start_date) and date(events.end_date)", date
+                                                                   ])
+      all_events += Event.all(conditions: [
+                                "(? between date(events.start_date) and date(events.end_date)) and is_exam = true", date
+                              ])
+      all_events += Event.all(conditions: [
+                                "(? between date(events.start_date) and date(events.end_date)) and is_common = true", date
+                              ])
     end
     all_events
   end
 
   def next_event(date)
-    all_events=[]
-    case(role_name)
+    all_events = []
+    case role_name
     when "Admin"
-      all_events=Event.find(:all,:conditions => ["? < date(events.end_date)",date],:order=>"start_date")
+      all_events = Event.find(:all, conditions: ["? < date(events.end_date)", date], order: "start_date")
     when "Student"
-      all_events+= events.all(:conditions=>["? < date(events.end_date)",date])
-      all_events+= student_record.batch.events.all(:conditions=>["? < date(events.end_date)",date],:order=>"start_date")
-      all_events+= Event.all(:conditions=>["(? < date(events.end_date)) and is_common = true",date],:order=>"start_date")
+      all_events += events.all(conditions: ["? < date(events.end_date)", date])
+      all_events += student_record.batch.events.all(conditions: ["? < date(events.end_date)", date],
+                                                    order: "start_date")
+      all_events += Event.all(conditions: ["(? < date(events.end_date)) and is_common = true", date],
+                              order: "start_date")
     when "Parent"
-      all_events+= events.all(:conditions=>["? < date(events.end_date)",date])
-      all_events+= parent_record.user.events.all(:conditions=>["? < date(events.end_date)",date])
-      all_events+= parent_record.batch.events.all(:conditions=>["? < date(events.end_date)",date],:order=>"start_date")
-      all_events+= Event.all(:conditions=>["(? < date(events.end_date)) and is_common = true",date],:order=>"start_date")
+      all_events += events.all(conditions: ["? < date(events.end_date)", date])
+      all_events += parent_record.user.events.all(conditions: ["? < date(events.end_date)", date])
+      all_events += parent_record.batch.events.all(conditions: ["? < date(events.end_date)", date],
+                                                   order: "start_date")
+      all_events += Event.all(conditions: ["(? < date(events.end_date)) and is_common = true", date],
+                              order: "start_date")
     when "Employee"
-      all_events+= events.all(:conditions=>["? < date(events.end_date)",date],:order=>"start_date")
-      all_events+= employee_record.employee_department.events.all(:conditions=>["? < date(events.end_date)",date],:order=>"start_date")
-      all_events+= Event.all(:conditions=>["(? < date(events.end_date)) and is_exam = true",date],:order=>"start_date")
-      all_events+= Event.all(:conditions=>["(? < date(events.end_date)) and is_common = true",date],:order=>"start_date")
+      all_events += events.all(conditions: ["? < date(events.end_date)", date], order: "start_date")
+      all_events += employee_record.employee_department.events.all(conditions: ["? < date(events.end_date)", date],
+                                                                   order: "start_date")
+      all_events += Event.all(conditions: ["(? < date(events.end_date)) and is_exam = true", date],
+                              order: "start_date")
+      all_events += Event.all(conditions: ["(? < date(events.end_date)) and is_common = true", date],
+                              order: "start_date")
     end
-    start_date=all_events.collect(&:start_date).min
-    unless start_date
-      return ""
+    start_date = all_events.collect(&:start_date).min
+    if start_date
+      (start_date.to_date <= date ? date + 1.day : start_date)
+
     else
-      next_date=(start_date.to_date<=date ? date+1.days : start_date )
-      next_date
+      ""
     end
   end
 
   def soft_delete
-    self.update_attributes(:is_deleted =>true)
+    update(is_deleted: true)
   end
 end
